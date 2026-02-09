b6c89145fe0ec9347b449e4e669c7382a62b09ac
Penyushkina Nadezda <najia00@qq.com>
2026-02-09 21:00:05 +0800

    DjangoORM

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,16 @@
+from django.db.models import Q
 from django.shortcuts import render
+from ice_cream.models import IceCream
 
 
 def index(request):
-    template = 'homepage/index.html'
-    return render(request, template)
+    template_name = 'homepage/index.html'
+    ice_cream_list = IceCream.objects.values(
+            'id', 'title', 'description'
+        ).filter(
+            Q(is_published=True) & Q(is_on_main=True)
+        ).order_by('title')[0:3]
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+    return render(request, template_name, context)
